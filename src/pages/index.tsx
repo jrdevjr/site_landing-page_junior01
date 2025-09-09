6d621d1313015fb95ecd6688e59bbbbe95633693
!jrdevjr <junioraguia2323@gmail.com>
2025-09-09 12:48:04 -0300

    header e links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,15 @@
-import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Header } from "@/components/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
-    <div className="min-h-screen min-w-full bg-slate-500">
-      <h2 className= "text-4xl text-red-400"> Junior </h2>
+    <div className="min-h-screen min-w-full bg-slate-500 ">
+    
+    <Header />
     </div>
   );
 }
