b44089c387d8a0c48257ce4675f9ef0bd0f04842
!jrdevjr <junioraguia2323@gmail.com>
2025-09-26 08:47:06 -0300

    primiero post e md

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 
 import { LandingPage } from "@/templates/landing-page";
+import { allPosts } from "contentlayer/generated";
 
 
 export default function Home() {
+  console.log(allPosts);
+  
   return (
     
     <LandingPage />
